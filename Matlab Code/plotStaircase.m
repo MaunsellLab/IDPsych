b7400fbe0e7bd391adb4e9033b1d810ac5aa3c92
John Maunsell <maunsell@uchicago.edu>
2022-07-23 15:11:47 -0500

    Processed data files

---
+++ Matlab Code/plotStaircase.m
@@ -0,0 +1,21 @@
+function plotStaircase(filePath)
+
+  load(filePath, 'file', 'trials');
+  trialStructs = [trials(:).trial];
+  eotCodes = [trials(:).eotCode];
+  stepCohPC = [trialStructs(:).stepCohPC];
+  stepDir = [trialStructs(:).stepDir];
+  correctIndices = find(eotCodes == 0);
+  wrongIndices = find(eotCodes == 1);
+
+  figure(1);
+  clf;
+  plot(correctIndices, stepCohPC(correctIndices), 'ko', 'MarkerFaceColor', 'c');
+  hold on;
+  plot(wrongIndices, stepCohPC(wrongIndices), 'ko', 'MarkerFaceColor', 'r');
+  axis([0, file.numberOfTrials, 0, 100]);
+  plot([0, file.numberOfTrials], trials(1).trial.baseCohPC * [1, 1], 'k');
+  ylabel('step coherence (%)');
+  xlabel('trial number');
+  title(filePath);
+end
